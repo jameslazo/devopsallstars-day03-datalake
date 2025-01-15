1d193b7e8232e8154523fd300bfa7d5b940bc203
jameslazo <james.lazo+git@protonmail.com>
2025-01-15 06:41:39 -0700

    another one

--- src/transform.py
+++ src/transform.py
@@ -4,6 +4,7 @@ import json
 import urllib.parse
 import boto3
 import os
+from datetime import datetime as dt
 
 transform_bucket_name = os.getenv("DEVOPS_PREFIX") + os.getenv("TRANSFORM_BUCKET_NAME")
 
@@ -12,6 +13,8 @@ transform_bucket_name = os.getenv("DEVOPS_PREFIX") + os.getenv("TRANSFORM_BUCKET
 print('Loading function')
 
 s3 = boto3.client('s3')
+
+today = dt.today().strftime("%Y-%m-%d")
 
 
 def lambda_handler(event, context):
@@ -28,7 +31,7 @@ def lambda_handler(event, context):
         line_delimited_data = "\n".join([json.dumps(record) for record in data])
 
         # Define S3 object key
-        file_key = "raw-data/nba_player_data.jsonl"
+        file_key = f"{today}/nba_player_data.jsonl"
 
         # Upload JSON data to S3
         s3.put_object(
